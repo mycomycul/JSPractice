3791419354eab50c960ff76044c23de09687bc1a
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-11-03 20:53:52 -0700

    Create first module

--- usefirstmodule.js
+++ usefirstmodule.js
@@ -1,5 +1,6 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 http.createServer(function (req, res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World!');
+    res.end("The date and time are ccurrently: " + dt.myDateTime());
 }).listen(8080);
