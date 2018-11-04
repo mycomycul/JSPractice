8e547603c1643941a9b19dae610f65a00d52d39f
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-11-04 15:55:37 -0800

    URL processing and fileserver samples

--- querystring.js
+++ querystring.js
@@ -1,10 +1,21 @@
 var http = require('http');
 var url = require('url');
-var dt = require('./myfirstmodule');
-http.createServer(function (req, res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.write(txt)
-    res.end(req.url);
-}).listen(8080);
+
+/*Basic query string processing*/
+// var dt = require('./myfirstmodule');
+// http.createServer(function (req, res){
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     var q = url.parse(req.url, true).query;
+//     var txt = q.year + " " + q.month;
+//     res.write(txt)
+//     res.end(req.url);
+// }).listen(8080);
+
+/*URL Processing*/
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+var qdata = q.query;
+console.log(qdata.month);
